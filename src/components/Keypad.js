26eaea690ce5eea5a0aa35d6b11557f08e1d20ca
muthoni <muthoni.ndehi@student.moringaschool.com>
2022-09-28 21:09:37 +0300

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,12 @@
 // Code Keypad Component Here
+import React from 'react' 
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+function Keypad() {
+    function handleSubmit(event){
+        console.log('Entering password...')
+    }
+        return (
+            <input type="password" onChange={handleSubmit} />
+        )}
 
-export default Keypad;
+export default Keypad
